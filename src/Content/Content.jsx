d0e846730cb5a21a742a8e17e29feb75b47eaf72
mehran <mehran.mj9999@gmail.com>
2021-01-07 18:20:36 +0330

    modal in windows size completed

--- src/Content/Content.jsx
+++ src/Content/Content.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment, useState } from 'react';
+import Modal from './Modal';
 
 const Content = () => {
     const [showModal, setShowModal] = useState(false)
@@ -11,15 +12,17 @@ const Content = () => {
             پسماند هایی است که قابلیت بازماند دارند. برای این کار به شما<br />
              نیاز داریم، با ما همراه شوید تا بهروب و ناوگان آن را در سراسر <br />
              ایران گسترش دهیم</p>
-             
+<Modal />
                     <div className="flex gap-5 custom-buttons">
                         <button
                             className="custom-contact px-8 py-4 text-white text-sm font-light focus:outline-none"
                             style={{ transition: 'all 0.15s ease' }}
+                            onClick={() => setShowModal(true)}
                         > تماس با ما </button>
-                            
-                            
-                            
+                        
+
+
+
                         <button className="custom-info px-8 py-4 text-black text-sm font-light focus:outline-none"> بیشتر بدانید </button>
                     </div>
                 </div>
