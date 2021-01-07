2122cc4d6eb54ceb3a4fd6a5e46bac1d059b1c55
mehran <mehran.mj9999@gmail.com>
2021-01-07 11:47:45 +0330

    first coloumn finished

--- src/App.js
+++ src/App.js
@@ -5,26 +5,28 @@ function App() {
   return (
     <div className='absolute custom-background'>
       <Icons />
-      <div className="fixed right-10 top-10">
+      <div className="absolute right-10 top-10">
         <label class="switch">
           <input type="checkbox" />
           <span class="round"></span>
         </label>
       </div>
       <div className='container box-border mx-auto relative top-16'>
-        <div class="h-64 grid grid-cols-2 justify-items-center custom-direction gap-4">
-          <div>
-            <h2 className="text-white text-right text-3xl font-extrabold mb-5">پوشش شهر ها</h2>
-            <p className="text-white leading-loose font-light text-sm text-right">هدف در نهایت حفظ محیط زیست با جمع آوری <br />
+        <div class="h-64 md:grid md:grid-cols-2 flex flex-col justify-items-center custom-direction md:gap-4">
+          <div className='mt-16 md:mt-0 grid justify-items-center'>
+            <div className="">
+              <h2 className="text-white text-right text-3xl font-extrabold mb-5">پوشش شهر ها</h2>
+              <p className="text-white leading-loose font-light text-sm text-right">هدف در نهایت حفظ محیط زیست با جمع آوری <br />
             پسماند هایی است که قابلیت بازماند دارند. برای این کار به شما<br />
              نیاز داریم، با ما همراه شوید تا بهروب و ناوگان آن را در سراسر <br />
              ایران گسترش دهیم</p>
-            <div className="flex gap-5 custom-buttons">
-              <button className="custom-contact px-8 py-4 text-white text-sm font-light focus:outline-none"> تماس با ما </button>
-              <button className="custom-info px-8 py-4 text-black text-sm font-light focus:outline-none"> بیشتر بدانید </button>
+              <div className="flex gap-5 custom-buttons">
+                <button className="custom-contact px-8 py-4 text-white text-sm font-light focus:outline-none"> تماس با ما </button>
+                <button className="custom-info px-8 py-4 text-black text-sm font-light focus:outline-none"> بیشتر بدانید </button>
+              </div>
             </div>
           </div>
-          <div className='border-solid border-2 p-10 rounded-2xl border-white relative'>
+          <div className='border-solid border-2 p-10 m-10 md:m-0 rounded-2xl border-white relative mt-8 md:mt-0'>
             <div className='absolute bottom-14 flex gap-4 left-10'>
               <span className="contents custom-span"> به زودی </span>
               <img src="./img/Path 207@2x.png" alt="path 207" className='z-10' />
@@ -33,7 +35,7 @@ function App() {
               <span className="contents custom-span"> شهر های فعال </span>
               <img src="./img/Path 206@2x.png" alt="path 206" className='z-10' />
             </div>
-            
+
             <img src="./img/Group 28@2x.png" className='' alt="group" />
             <img src="./img/Path 207@2x.png" alt="path" className='absolute top-80 left-64 cursor-pointer' />
             <img src="./img/Path 207@2x.png" alt="path" className='absolute top-56 left-56 cursor-pointer' />
--- src/Icons/Icons.jsx
+++ src/Icons/Icons.jsx
@@ -3,13 +3,15 @@ import React, { Fragment } from 'react';
 const Icons = () => {
     return (
         <Fragment>
-            <img src="./img/wind@2x.png" alt="wind big" className='fixed md:flex hidden top-20 -right-80' />
-            <img src="./img/wind.png" alt="wind small" className='fixed md:hidden flex top-48 -right-40' />
-            <img src="./img/recycle.png" alt="recycle" className='fixed custom-recycle-position' />
-            <img src="./img/mountain.png" alt="mountain" className='fixed custom-mountain-position' />
-            <img src="./img/wind.png" alt="wind" className='fixed custom-wind-position' />
-            <img src="./img/Ellipse 2@2x.png" alt="Ellipse 2 big" className='fixed md:flex hidden top-10 w-40 -left-20' />
-            <img src="./img/Ellipse 2.png" alt="Ellipse 2 small" className='fixed top-48 flex md:hidden w-20 -left-10' />
+            <img src="./img/wind@2x.png" alt="wind big" className='absolute md:flex hidden top-20 -right-80' />
+            <img src="./img/wind.png" alt="wind small" className='absolute md:hidden flex top-48 -right-40' />
+            <img src="./img/recycle@2x.png" alt="recycle big" className='absolute md:flex hidden top-80 -right-20 w-60' />
+            <img src="./img/recycle.png" alt="recycle small" className='absolute flex md:hidden c-top-big -right-12 w-28' />
+            <img src="./img/mountain.png" alt="mountain" className='absolute custom-mountain-position' />
+            <img src="./img/wind@2x.png" alt="wind big" className='absolute hidden md:flex top-80 -left-36 w-72' />
+            <img src="./img/wind.png" alt="wind small" className='absolute flex md:hidden -left-32 c-top-big' />
+            <img src="./img/Ellipse 2@2x.png" alt="Ellipse 2 big" className='absolute md:flex hidden top-10 w-40 -left-20' />
+            <img src="./img/Ellipse 2.png" alt="Ellipse 2 small" className='absolute top-48 flex md:hidden w-20 -left-10' />
         </Fragment>
     );
 }
