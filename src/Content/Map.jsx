1ee6928d6cda200bfb872b99e98a005a4f176db0
mehran <mehran.mj9999@gmail.com>
2021-01-07 13:08:02 +0330

    responsive mod finished

--- src/Content/Map.jsx
+++ src/Content/Map.jsx
@@ -4,13 +4,15 @@ const Map = () => {
     return (
         <Fragment>
             <div className='border-solid border-2 p-10 m-10 md:m-0 rounded-2xl border-white relative mt-8 md:mt-0'>
-                <div className='absolute bottom-14 flex gap-4 left-10'>
-                    <span className="contents custom-span"> به زودی </span>
-                    <img src="./img/Path 207@2x.png" alt="path 207" className='z-10' />
+                <div className='absolute sm:bottom-14 bottom-4 left-8 flex gap-4 sm:left-10'>
+                    <span className="contents custom-span text-white font-normal text-xs"> به زودی </span>
+                    <img src="./img/Path 207@2x.png" alt="path 207 lg" className='z-10 hidden sm:flex w-8' />
+                    <img src="./img/Path 207.png" alt="path 207 sm" className='z-10 flex sm:hidden w-4' />
                 </div>
-                <div className='absolute bottom-24 flex gap-4 left-10'>
-                    <span className="contents custom-span"> شهر های فعال </span>
-                    <img src="./img/Path 206@2x.png" alt="path 206" className='z-10' />
+                <div className='absolute sm:bottom-24 bottom-12 flex gap-4 sm:left-10 left-8'>
+                    <span className="contents custom-span text-white font-normal text-xs"> شهر های فعال </span>
+                    <img src="./img/Path 206@2x.png" alt="path 206 lg" className='z-10 hidden sm:flex w-8' />
+                    <img src="./img/Path 206.png" alt="path 206 sm" className='z-10 flex sm:hidden w-4' />
                 </div>
 
                 <img src="./img/Group 28@2x.png" className='' alt="group" />
