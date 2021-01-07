39321d57bb82cf7a52acd67a4124e31d464a1642
mehran <mehran.mj9999@gmail.com>
2021-01-07 13:23:34 +0330

    responsive mode finished

--- src/Content/Map.jsx
+++ src/Content/Map.jsx
@@ -5,12 +5,12 @@ const Map = () => {
         <Fragment>
             <div className='border-solid border-2 p-10 m-10 md:m-0 rounded-2xl border-white relative mt-8 md:mt-0'>
                 <div className='absolute sm:bottom-14 bottom-4 left-8 flex gap-4 sm:left-10'>
-                    <span className="contents custom-span text-white font-normal text-xs"> به زودی </span>
+                    <span className="contents custom-span text-white font-normal text-xs sm:text-sm"> به زودی </span>
                     <img src="./img/Path 207@2x.png" alt="path 207 lg" className='z-10 hidden sm:flex w-8' />
                     <img src="./img/Path 207.png" alt="path 207 sm" className='z-10 flex sm:hidden w-4' />
                 </div>
                 <div className='absolute sm:bottom-24 bottom-12 flex gap-4 sm:left-10 left-8'>
-                    <span className="contents custom-span text-white font-normal text-xs"> شهر های فعال </span>
+                    <span className="contents custom-span text-white font-normal text-xs sm:text-sm"> شهر های فعال </span>
                     <img src="./img/Path 206@2x.png" alt="path 206 lg" className='z-10 hidden sm:flex w-8' />
                     <img src="./img/Path 206.png" alt="path 206 sm" className='z-10 flex sm:hidden w-4' />
                 </div>
