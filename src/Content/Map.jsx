5111def9408a7b9990e4e97dcc0302615e2bc2d7
mehran <mehran.mj9999@gmail.com>
2021-01-09 00:12:09 +0330

    mashhad modal set

--- src/Content/Map.jsx
+++ src/Content/Map.jsx
@@ -1,6 +1,8 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
+import Town from './Towns';
 
-const Map = ({dark}) => {
+const Map = ({ dark }) => {
+    const [mashhad, setMashhad] = useState(false)
     return (
         <Fragment>
             <div className='border-solid border-2 p-10 m-10 md:m-0 rounded-2xl border-white relative mt-8 md:mt-0'>
@@ -14,7 +16,6 @@ const Map = ({dark}) => {
                     <img src="./img/Path 206@2x.png" alt="path 206 lg" className='z-10 hidden sm:flex w-8' />
                     <img src="./img/Path 206.png" alt="path 206 sm" className='z-10 flex sm:hidden w-4' />
                 </div>
-
                 <img src={dark ? './img/Group 28@2x.png' : './img/Group -1@2x.png'} className='c-transition' alt="group" />
                 <img src="./img/Path 207@2x.png" alt="path lg" className='absolute top-80 left-64 cursor-pointer hidden sm:flex' />
                 <img src="./img/Path 207.png" alt="path sm" className='absolute top-40 left-32 cursor-pointer flex sm:hidden' />
@@ -26,7 +27,15 @@ const Map = ({dark}) => {
                 <img src="./img/Path 207.png" alt="path sm" className='absolute top-20 left-28 cursor-pointer flex sm:hidden' />
                 <img src="./img/Path 206@2x.png" alt="path lg" className='absolute top-64 left-64 cursor-pointer hidden sm:flex' />
                 <img src="./img/Path 206.png" alt="path sm" className='absolute top-32 left-36 cursor-pointer flex sm:hidden' />
-                <img src="./img/Path 206@2x.png" alt="path lg" className='absolute top-36 left-96 cursor-pointer hidden sm:flex' />
+                <div className="sm:-top-3/4 sm:left-1/10 hidden relative sm:flex">
+                    <img
+                        src="./img/Path 206@2x.png"
+                        alt="path lg"
+                        className={`absolute sm:top-1/4 sm:left-2/3 cursor-pointer ${mashhad ? '' : 'ring-4'}`}
+                        onClick={() => setMashhad(!mashhad)}
+                    />
+                    <Town hide={mashhad} dark={dark} />
+                </div>
                 <img src="./img/Path 206.png" alt="path sm" className='absolute top-20 left-48 cursor-pointer flex sm:hidden' />
                 <img src="./img/Path 206@2x.png" alt="path lg" className='absolute top-40 left-20 cursor-pointer hidden sm:flex' />
                 <img src="./img/Path 206.png" alt="path sm" className='absolute top-24 left-20 cursor-pointer flex sm:hidden' />
